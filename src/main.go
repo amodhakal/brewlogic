801b9b760fcb1893415d91ea350f061b766c0913
Amodh Dhakal <amodhakal@gmail.com>
2025-12-03 23:12:17 -0500

    Setup go fiber and its middlewares

--- src/main.go
+++ src/main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
-	"amodhakal/brewlogic/src/config"
 	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/healthcheck"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/monitor"
 )
 
 func main() {
-	result, err := config.Query.GetUsers(config.DBContext)
-	if err != nil {
-		log.Fatal(err)
-	}
+	app := fiber.New()
+	app.Use(healthcheck.New())
+	app.Use(logger.New(logger.Config{Format: "[${ip}]:${port} ${status} - ${method} ${path}\n"}))
+	app.Get("/metrics", monitor.New())
 
-	log.Println(result)
+	log.Fatal(app.Listen(":3000"))
 }
